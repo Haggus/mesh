eb2c95f823404d82105c3ba2cdef412973639427
Mateusz 'Haggus' Mrowiec <matt.mrowiec@gmail.com>
2016-04-14 19:35:47 +0200

    Added React and gulp tasks to handle the builds

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,37 @@
 'use strict';
 
+var fs = require('fs');
 var gulp = require('gulp');
 var electron = require('electron-connect').server.create();
+var browserify = require('browserify');
+var babelify = require('babelify');
 
-gulp.task('serve', function () {
+var config = {
+    paths: {
+        html: './src/index.html',
+        js: './src/**/*.js',
+        app: './src/app.js',
+        main: './src/main.js',
+    }
+};
 
-  // Start browser process
-  electron.start();
+gulp.task('js', function() {
+    return browserify(config.paths.main)
+        .transform(babelify, {presets: ['es2015', 'react']})
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(fs.createWriteStream('build/bundle.js'));
+});
 
-  // Restart browser process
-  gulp.watch('src/app.js', electron.restart);
+gulp.task('build_js', ['js'], function() {
+    electron.start();
+})
 
-  // Reload renderer process
-  gulp.watch(['src/index.html'], electron.reload);
+gulp.task('rebuild_js', ['js'], function() {
+    electron.reload();
+});
+
+gulp.task('serve', ['build_js'], function () {
+    gulp.watch(config.paths.app, electron.restart);
+    gulp.watch(config.paths.js, ['rebuild_js']);
 });
